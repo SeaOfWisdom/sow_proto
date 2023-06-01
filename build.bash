7e1e4597cff38da5aae0273aac152347a4c210f5
Vladimir Volog <vvv@oreolo.com>
2023-06-01 14:19:47 +0530

    proto files cosmetics

--- build.bash
+++ build.bash
@@ -1,2 +1,40 @@
-#!/bin/bash
-protoc -I./ -I/usr/local/include --go_out=./ --go-grpc_out=./ ./*.proto
+#!/bin/sh                                                                                                                                                                                                   
+MODULE_PWD=$PWD
+SERVICES_PWD=services
+MESSAGES_PWD=messages
+
+cd ${MODULE_PWD}/${MESSAGES_PWD}
+
+for d in *; do
+    if ! [ -d $d ]; then
+        continue
+    fi
+
+    if [ -f ./$d/$d.proto ]; then
+        protoc -I. \
+               -I../ \
+               --go_out=. \
+               --go-grpc_out=. \
+               --go_opt=paths=source_relative \
+               $d/$d.proto
+        echo "messages: $d.proto"
+    fi
+done
+
+cd ${MODULE_PWD}/${SERVICES_PWD}
+
+for d in *; do
+    if ! [ -d $d ]; then
+        continue
+    fi
+
+    if [ -f ./$d/$d.proto ]; then
+		  protoc -I. \
+               -I../ \
+               --go_out=. \
+               --go-grpc_out=. \
+               --go_opt=paths=source_relative \
+               $d/$d.proto
+        echo "services: $d.proto"
+    fi
+done
